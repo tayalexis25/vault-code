7d50bcfe2bd42886ac9adf1ea9db173600d82f42
Taylor Hirth <taylorhirth19@gmail.com>
2025-02-18 13:03:18 -0600

    added 2 complete files

---
+++ block07.js
@@ -0,0 +1,11 @@
+// i will be creating a string for the user saying: You have received this message because you have been chosen to open an important vault. Here is the secret combination:. this will create a message in the alert.
+
+let message = "You have received this message because you have been chosen to open an important vault. This is the combination:";
+
+//assigning three variables for each number that makes up the combination using different mathmatic symbols to achieve the correct numbers.
+let num1 = 5 * 2;
+let num2 = 50 - 10;
+let num3 = 33 + 6;
+
+// alert message will pop up displaying the combination of the three previous variables, along with the message above.
+alert(`${message}-${num1}-${num2}-${num3}`);
